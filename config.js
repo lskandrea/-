7361ac07a4ebd091dc01d18dfee2c099fd980f60
lskandrea <157403455+lskandrea@users.noreply.github.com>
2026-06-14 15:31:51 +0200

    Fix address formatting and update Google Maps URL
    Corrected the address capitalization and updated the Google Maps URL format.

--- config.js
+++ config.js
@@ -1,23 +1,19 @@
-// Remplace les valeurs ci-dessous par les vrais liens du club.
 window.MJSC_CONFIG = {
   clubName: "MJSC Judo Manosque",
-  address: "Lycée des iscles, Manosque",
+  address: "Lycée des Iscles, Manosque",
   email: "contact@mjsc-judo.fr",
-  phone: "+33 7 82 97 65 59"
-  googleMapsUrl: "https://www.bing.com/maps/search?mepi=0%7E%7EEmbedded%7EAddress_Link&ty=18&v=2&sV=1&FORM=MPSRPL&q=Lyc%C3%A9e+Polyvalent+Les+Iscles&ss=id.ypid%3AYNBBFB30DD3E3C026A&ppois=43.82978057861328_5.797555923461914_Lyc%C3%A9e+Polyvalent+Les+Iscles_YNBBFB30DD3E3C026A%7E&cp=43.829781%7E5.797556&lvl=11&style=r",
+  phone: "+33 7 82 97 65 59",
 
-  // Google Calendar : rends le calendrier public puis copie le lien d'intégration iframe.
+  googleMapsUrl: "https://www.google.com/maps/search/?api=1&query=Lycée%20des%20Iscles%20Manosque",
+
   googleCalendarEmbedUrl: "https://calendar.google.com/calendar/embed?src=fr.french%23holiday%40group.v.calendar.google.com&ctz=Europe%2FParis",
 
-  // Google Forms : crée un formulaire puis colle ici l'URL /viewform?embedded=true
- googleFormEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLScWsSrNFTJjsfwFfQDwocRDX1ryQ00JIu_pEe0zPilvZBVaEA/viewform?embedded=true",
-  // Google Drive : dossier partagé pour documents d'inscription.
+  googleFormEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLScWsSrNFTJjsfwFfQDwocRDX1ryQ00JIu_pEe0zPilvZBVaEA/viewform?embedded=true",
+
   googleDriveFolderUrl: "https://drive.google.com/drive/folders/1xPzKkZ_tdigLaPH5XZI_ixVt4WO3Wf5f?usp=sharing",
 
   facebookUrl: "https://www.facebook.com/",
   instagramUrl: "https://www.instagram.com/",
 
-  // YouTube : mets l'ID d'une vidéo ou d'une playlist.
   youtubeVideoId: "dQw4w9WgXcQ"
 };
-+
